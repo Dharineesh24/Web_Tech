2e0f442fc22418c11150016cdf5b157de1b12393
Dharineesh A <dharineeshmohan2424@gmail.com>
2026-01-08 15:47:49 +0530

    Add files via upload

---
+++ JAVASCRIPT/5_function/function.js
@@ -0,0 +1,41 @@
+
+function lion() {
+    console.log("The lion is the king of the jungle..!");
+
+}
+lion(); // Calling the function to see the output
+
+
+
+
+//with parametersfunction 
+function add(a,b) {
+    console.log(a);
+    console.log(b);
+    console.log(a+b);
+
+}
+add(100,250); // Calling the function with arguments,
+
+
+function multiply(x,y,z) {
+    console.log(x);
+    console.log(y);
+    console.log(z);
+    console.log(x*y*z);
+}
+multiply(10,20,30); // Calling the function with arguments,
+
+
+
+
+function addd(c,d){
+
+    console.log("Function without parameters and without return type");
+
+    return c + d; // return type
+}
+
+let adddResult = addd(100,250); // Calling the function
+
+console.log(adddResult); // printing the return value  
